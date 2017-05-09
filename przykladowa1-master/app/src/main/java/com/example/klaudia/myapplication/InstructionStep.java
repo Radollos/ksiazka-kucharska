bdc76260134b8977c27bb83e922fddbbb0c41bf9
Radollos <radek24_95@o2.pl>
2017-05-09 03:25:35 +0200

    Dalszy rozwój struktury

---
+++ przykladowa1-master/app/src/main/java/com/example/klaudia/myapplication/InstructionStep.java
@@ -0,0 +1,22 @@
+package com.example.klaudia.myapplication;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Radek on 2017-05-09.
+ */
+
+public class InstructionStep {
+    private int number;
+    private String step;
+    private ArrayList<Ingredient> ingredients;
+//    private
+
+
+    public InstructionStep(int number, String step, ArrayList<Ingredient> ingredients){
+        this.number = number;
+        this.step = step;
+        this.ingredients = ingredients;
+//        this.
+    }
+}
