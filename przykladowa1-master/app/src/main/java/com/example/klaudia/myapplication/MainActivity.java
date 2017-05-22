228fb3e722497e1e339f4da1faf4d15d838e65dc
Klaudia2416 <klaudia.herkt@wp.pl>
2017-05-22 20:32:23 +0200

    Co jest zrobione: 1. Wyszukiwanie po tagach, 2. Wyszukiwanie podobnych przepisow, 3. Wyszukiwanie zaawansowane, 4. Wyszukiwanie po skladnikach, 5. Pobieranie obrazkow z jsona przepisu 6. Adapter do list view wyswietlajacy obrazek i tytul dania
    Klasa MainKlaudia to moja klasa do testowania.

--- przykladowa1-master/app/src/main/java/com/example/klaudia/myapplication/MainActivity.java
+++ przykladowa1-master/app/src/main/java/com/example/klaudia/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.klaudia.myapplication;
+import android.util.Log;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -9,7 +13,9 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,87 +23,137 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.HashMap;
 
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
+{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
     private GridView gridview;
+    Searcher searcher;
+    HashMap<String, Bitmap> results;
+    SearchView searchView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        searcher = new Searcher();
+        results = new HashMap<String, Bitmap>();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle (
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        createGridView();
+        handleIntent(getIntent());
 
-         createGridView();
     }
 
-    private void createGridView(){
+    private void createGridView()
+    {
         gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(getApplicationContext(), "You chose " + position,
-                        Toast.LENGTH_SHORT).show();
+                                    int position, long id)
+            {
+               switch (position)
+               {
+                   case 0:
+                       results = searcher.tagsSearch_TitlesImages("breakfast");
+                       break;
+                   case 1:
+                       results = searcher.tagsSearch_TitlesImages("dessert");
+                       break;
+                   case 2:
+                       results = searcher.tagsSearch_TitlesImages("fish");
+                       break;
+                   case 3:
+                       results = searcher.tagsSearch_TitlesImages("meat");
+                       break;
+                   case 4:
+                       results = searcher.tagsSearch_TitlesImages("soup");
+                       break;
+                   case 5:
+                       results = searcher.tagsSearch_TitlesImages("vegetarian");
+                       break;
+               }
             }
         });
     }
 
     @Override
-    public void onBackPressed() {                                                                   // setting nav drawer
+    public void onBackPressed()
+    {                                                                   // setting nav drawer
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+        {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+        }
+        else
+        {
             super.onBackPressed();
         }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
+//        super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.main, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.fast_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.fast_search) {
-            return true;
+        switch (id)
+        {
+            //nie dziala takie rozwiazenie do szukania, wtf
+            case R.id.fast_search:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -122,4 +178,22 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    protected void onNewIntent(Intent intent)
+    {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent)
+    {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction()))
+        {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            results = searcher.tagsSearch_TitlesImages(query);
+        }
+    }
+
+
 }
