5c293de7646022df5e6c2f17ecb202725381e223
Adriano512 <adriano512@gmail.com>
2017-06-04 23:31:57 +0200

    internal storage -> moze sie przyda

---
+++ przykladowa1-master/app/src/main/java/com/example/klaudia/myapplication/InternalStorage.java
@@ -0,0 +1,34 @@
+package com.example.klaudia.myapplication;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Created by Admin on 2017-05-31.
+ */
+
+public final class InternalStorage{
+
+    private InternalStorage() {}
+
+    public static void save(Context context, String key, Object object) throws IOException {
+        FileOutputStream fos = context.openFileOutput(key, Context.MODE_PRIVATE);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(object);
+        oos.close();
+        fos.close();
+    }
+
+    public static Object get(Context context, String key) throws IOException,
+            ClassNotFoundException {
+        FileInputStream fis = context.openFileInput(key);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Object object = ois.readObject();
+        return object;
+    }
+}
